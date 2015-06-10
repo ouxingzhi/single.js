c765ac1441cd92d4310d1ae4bddf2212ca01ff52
安顿 <ouxingzhi@vip.qq.com>
2015-06-10 23:55:32 +0800

    update

---
+++ src/base/date.js
@@ -0,0 +1,134 @@
+define(function(require){
+	var Class = require('base/class');
+
+	var regTime = /[a-z]/ig;
+
+	var DDate = Class({
+		propertys:function(){
+			this.date;
+		},
+		initialize:function(date){
+			DDate.parse(date||new Date());
+		}
+	});
+
+	var FORMAT_HANDLE = {
+		//日期:dd
+		d:function(date){
+			var d = date.getDate();
+			return d < 10 ? '0'+d : d;
+		},
+		//日期:d
+		j:function(date){
+			return date.getDate();
+		},
+		//周几 d
+		N:function(date){
+			var d = date.getDay();
+			return d === 0 ? 7 : d;
+		},
+		//周几
+		w:function(date){
+			return date.getDay();
+		},
+		//月份，有前导零
+		m:function(date){
+			var m = date.getMonth()+1;
+			return m < 10 ? '0'+ m : m; 
+		},
+		//月份,无前导零
+		n:function(date){
+			return date.getMonth()+1;
+		},
+		//年份 0000
+		Y:function(date){
+			return date.getFullYear();
+		},
+		//年份 00
+		y:function(date){
+			return String(date.getFullYear()).replace(/^\d{2}/,'');
+		},
+		//小时，12 小时格式，没有前导零
+		g:function(date){
+			var h = date.getHours();
+			return h > 12 ? h - 12 : h;
+		},
+		//小时，24 小时格式，没有前导零
+		G:function(date){
+			return date.getHours();
+		},
+		//小时，12 小时格式，有前导零
+		h:function(date){
+			var h = date.getHours();
+			h = h > 12 ? h - 12 : h;
+			return h < 10 ? '0'+h : h;
+		},
+		//小时，24 小时格式，有前导零
+		H:function(date){
+			var h = date.getHours();
+			return h < 10 ? '0'+h : h;
+		},
+		//有前导零的分钟数
+		i:function(date){
+			var m = date.getMinutes();
+			return m < 10 ? '0'+m : m;
+		},
+		//秒数，有前导零
+		s:function(date){
+			var s = date.getSeconds();
+			return s < 10 ? '0'+s : s;
+		},
+		//今天，昨天
+		O:function(date){
+			var a = new Date();
+			var diff = DDate.diffDay(a,date);
+			var map = {
+				'1':'昨天',
+				'0':'今天'
+			};
+			return map[diff] || '';
+		}
+	};
+
+	DDate.format = function(date,formatStr){
+		return formatStr.replace(regTime,function(key){
+			if(FORMAT_HANDLE[key]){
+				return FORMAT_HANDLE[key](date);
+			}
+			return key;
+		});
+	}
+
+	DDate.diffDay = function(a,b){
+		a = new Date(a.valueOf());
+		b = new Date(b.valueOf());
+		a.setHours(0,0,0,1);
+		b.setHours(0,0,0,1);
+		var diff = (a - b) / 864e5;
+		return diff;
+	};
+
+	DDate.parse = function(str){
+		if(typeof str === 'number') return new Date(str);
+		return new Date(Date.parse(str.replace(/-/g,'/')));
+	}
+	/**
+	 * 计算两个时间之间差值
+	 */
+	var D = 864e5,H = 36e5,I = 6e4;
+	DDate.diffFormat = function(a,base){
+		base = base || new Date();
+		var diff = base - a;
+		var d = parseInt(diff/D),
+			h = parseInt(diff%D/H),
+			i = parseInt(diff%H/I);
+		return {
+			d:d,
+			h:h,
+			i:i
+		};
+	}
+
+
+	return DDate;
+});
---
+++ src/common/lrc.js
@@ -0,0 +1,100 @@
+define(function(require){
+
+	/**
+	 * 音乐播放组件
+	 * @author xinzhi.oxz/安顿
+	 */
+
+	var Event = require('common/event');
+
+	var regline = /[\r\n]+/,
+		regmeta = /^\[(ti|ar|al|by|offset):([^\[\]]*)\]$/,
+		regtrim = /^\s+|\s+$/,
+		regtime = /((?:\[\d{1,2}:\d{1,2}(?:[:\.]\d{1,3}?)\])(?:\s*\[\d{1,2}:\d{1,2}(?:[:\.]\d{1,3}?)\])*)/,
+		regitemtime = /\[(\d{1,2}:\d{1,2}(?:[:\.]\d{1,3}?))\]/g,
+		regnum = /^0+([0-9]+)$/;
+
+	function parseTime(time){
+		var ts = time.split(/[:\.]/);
+		return parseInt(ts[0].replace(regnum,'$1')) * 60 + parseInt(ts[1].replace(regnum,'$1')) + parseInt((ts[2] || '0').replace(regnum,'$1'))/1000;
+	}
+
+	function parseTimeItem(str){
+		var ts = [];
+		str.replace(regitemtime,function(a,b){
+			ts.push(parseTime(b));
+		});
+		return ts;
+	}
+
+	function parseLrc(lrc){
+		lrc = lrc || '';
+		var lines = lrc.split(regline);
+		var p = [],time,line,meta={},matchs;
+		for(var i=0,l=lines.length;i<l;i++){
+			lines[i] = lines[i].replace(regtrim,'');
+			if(matchs = lines[i].match(regmeta)){
+				meta[matchs[1]] = matchs[2] || '';
+			}else if(matchs = lines[i].match(regtime)){
+				time = parseTimeItem(matchs[1]);
+				line = lines[i].replace(regtime,'');
+				for(var ii=0,ll=time.length;ii<ll;ii++){
+					p.push({
+						time:time[ii],
+						line:line,
+						id:(+new Date()) + String(Math.random()).replace('.','')
+					});
+				}
+				
+			}
+			
+		}
+		p.sort(function(a,b){
+			return a.time - b.time;
+		})
+		return {
+			meta:meta,
+			lines:p
+		};
+	} 
+
+
+	var Lrc = Event.extend({
+		propertys:function(){
+			this.lrc = '';
+			this.lrcObj;
+		},
+		initialize:function(options){
+			this._setOption(options);
+		},
+		_setOption:function(options){
+			if(options.lrc){
+				this.setLrc(options.lrc);
+			}
+		},
+		setLrc:function(lrc){
+			this.lrcObj = parseLrc(lrc);
+			this.emit('update',[this.lrcObj]);
+		},
+		getLrc:function(){
+			return this.lrcObj;
+		},
+		pos:function(time){
+			if(!this.lrcObj || !this.lrcObj.lines) return null;
+			var lines = this.lrcObj.lines;
+			var a,b;
+			var O = {};
+			for(var i=0,l=lines.length;i<l;i++){
+				a = lines[i];
+				b = lines[i+1] || O;
+				if(a.time <= time && (b === O || b.time > time) || (i==0 && a.time >= time)){
+					return a;
+				}
+			}
+			return null;
+		}
+	});
+	Lrc.parse = parseLrc;
+
+	return Lrc;
+});
